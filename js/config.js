98cd50c116898aecea20eb62cb9f355765ef4e38
izunigacodes <airdroplinux@gmail.com>
2025-10-31 18:18:05 -0300

    iniciar

---
+++ js/config.js
@@ -0,0 +1,16 @@
+// =====================
+// CONFIGURACIÓN GLOBAL
+// =====================
+const CONFIG = {
+  sitio: {
+    nombre: "Mi Sitio Web",
+    logo: "assets/img/logo.png", // opcional
+  },
+
+  menu: [
+    { texto: "Inicio", pagina: "home" },
+    { texto: "Acerca de", pagina: "acerca" },
+    { texto: "Contacto", pagina: "contacto" },
+    { texto: "Privacidad", pagina: "privacidad" },
+  ]
+};
